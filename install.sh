4ca8b4fab6f1f34bdb074eef9048e500d306aede
Erich Quintero <qbantek@gmail.com>
2019-10-11 15:21:27 -0400

    Installer to update repo from git

--- install.sh
+++ install.sh
@@ -1,3 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
+{ # This ensures the entire script is downloaded.
 
-echo "Hello World!"
+set -eo pipefail
+
+basedir="$HOME/projects/dotfiles"
+repourl="git@github.com:qbantek/dotfiles.git"
+
+function symlink() {
+  src="$1"
+  dest="$2"
+
+  if [ -e "$dest" ]; then
+    if [ -L "$dest" ]; then
+      if [ ! -e "$dest" ]; then
+        echo "Removing broken symlink at $dest"
+        rm "$dest"
+      else
+        # Already symlinked -- I'll assume correctly.
+        return 0
+      fi
+    else
+      # Rename files with a ".old" extension.
+      echo "$dest already exists, renaming to $dest.old"
+      backup="$dest.old"
+      if [ -e "$backup" ]; then
+        echo "Error: "$backup" already exists. Please delete or rename it."
+        exit 1
+      fi
+      mv -v "$dest" "$backup"
+    fi
+  fi
+  ln -sf "$src" "$dest"
+}
+
+if [ -d "$basedir/.git" ]; then
+  echo "Updating dotfiles using existing git..."
+  cd "$basedir"
+  git pull --quiet --rebase origin master || exit 1
+else
+  echo "Checking out dotfiles using git..."
+  rm -rf "$basedir"
+  git clone --quiet --depth=1 "$repourl" "$basedir"
+fi
+
+cd "$basedir"
+
+}
