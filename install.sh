d36bbc1885380d89f88765f3ff11909b5333e2c7
Erich N Quintero <qbantek@gmail.com>
2022-02-26 00:22:06 -0500

    Remove comments in code

--- install.sh
+++ install.sh
@@ -1,50 +1,50 @@
 # install brew
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # disable analytics
 brew analytics off
 
 # install iTerm2
-#brew install iterm2
+brew install iterm2
 
 # install git
-#brew install git
+brew install git
 
 # install nvm 0.39.1 and latest lts node
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-#nvm install --lts
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
 
 # have brew manage zsh installation
-#brew install zsh
+brew install zsh
 
 # install oh-my-zsh
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # powerlevel10k theme
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-#	${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+	${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
 
 # zsh-syntax-highlighting plugin
-#brew install zsh-syntax-highlighting
+brew install zsh-syntax-highlighting
 
 # stow
-#brew install stow
+brew install stow
 
 # bat
-#brew install bat
+brew install bat
 
 # neovim
-#brew install neovim --HEAD
+brew install neovim --HEAD
 
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # vscode
-#brew install --cask visual-studio-code
+brew install --cask visual-studio-code
 
 # docker
-#brew install --cask docker
+brew install --cask docker
