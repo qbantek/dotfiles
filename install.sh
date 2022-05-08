6b762e2558c2085b601382fec74ccba70bee2333
Erich N Quintero <qbantek@gmail.com>
2022-05-08 18:37:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,67 +59,71 @@
 #   git clone --quiet --depth=1 "$repourl" "$basedir"
 # fi
 
-# zsh
-brew install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-  "$ZSH_CUSTOM/themes/powerlevel10k"
-git clone https://github.com/zsh-users/zsh-autosuggestions \
-  "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+# # zsh
+# brew install zsh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+#   "$ZSH_CUSTOM/themes/powerlevel10k"
+# git clone https://github.com/zsh-users/zsh-autosuggestions \
+#   "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+#   "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
-# tmux
-brew install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# # tmux
+# brew install tmux
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# nvm 0.39.1 and latest lts node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install --lts
-npm install --global npm@latest
-npm install --global yarn
-PATH="$PATH:`yarn global bin`"
+# # nvm 0.39.1 and latest lts node
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# nvm install --lts
+# npm install --global npm@latest
+# npm install --global yarn
+# PATH="$PATH:`yarn global bin`"
 
-# go
-brew install golang
+# # go
+# brew install golang
 
-# python
-brew install python
-python3 -m pip install --upgrade pip
+# # python
+# brew install python
+# python3 -m pip install --upgrade pip
 
-# rvm
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg2 --import-ownertrust # mpapis@gmail.com
-echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg2 --import-ownertrust # piotr.kuczynski@gmail.com
-\curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+# # rvm
+# curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+# curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+# echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg2 --import-ownertrust # mpapis@gmail.com
+# echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg2 --import-ownertrust # piotr.kuczynski@gmail.com
+# \curl -sSL https://get.rvm.io | bash -s stable
+# source ~/.rvm/scripts/rvm
 
-# neovim
-brew install neovim --HEAD
-sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  python3 -m pip install --user --upgrade pynvim
+# # neovim
+# brew install neovim --HEAD
+# sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+#   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#   python3 -m pip install --user --upgrade pynvim
 
-# tools
-brew install iterm2
-brew install tree # directory structure as a 'tree'
-brew install bat  # cooler cat
-brew install ripgrep
-brew install fd   # simple, fast and user-friendly alternative to find
-brew install fzf  # fuzzy finder
-sh -c '/opt/homebrew/opt/fzf/install'
-brew install --cask visual-studio-code
-brew install --cask docker
-brew install --cask google-chrome
-brew install --cask postman
+# # tools
+# brew install iterm2
+# brew install tree # directory structure as a 'tree'
+# brew install bat  # cooler cat
+# brew install ripgrep
+# brew install fd   # simple, fast and user-friendly alternative to find
+# brew install fzf  # fuzzy finder
+# sh -c '/opt/homebrew/opt/fzf/install'
+# brew install --cask visual-studio-code
+# brew install --cask docker
+# brew install --cask google-chrome
+# brew install --cask postman
 
-# stow
-brew install stow
+# # stow
+# brew install stow
+
+basedir="$HOME/dotfiles"
+cd "$basedir"
+
 stow gem
 stow git
 stow nvim
