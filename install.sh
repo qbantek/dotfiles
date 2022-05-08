cbc562ec2327447431da4c70a4868fd3bf574f14
Erich N Quintero <qbantek@gmail.com>
2022-05-08 14:21:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ brew install gnupg  # make sure we have gpg available
 # mac
 if test "$(uname)" = "Darwin"; then 
   sudo softwareupdate --install --all
-  check=$((xcode-\select --install) 2>&1)
+  check=$((sudo xcode-\select --install) 2>&1)
   check=$((sudo xcodebuild -license accept) 2>&1)
 
   # show hidden files
