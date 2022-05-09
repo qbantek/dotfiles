6eb736e8ee2ca661ac03003100ff0f3572290d1b
Erich N Quintero <qbantek@gmail.com>
2022-05-08 20:02:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,127 +1,126 @@
 #!/bin/sh
 
 { # This ensures the entire script is downloaded.
-# # brew & tools
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# brew analytics off
-# brew update
-# brew install gnupg  # make sure we have gpg available
+# brew & tools
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew analytics off
+brew update
 
-# # mac
-# sudo softwareupdate --install --all
-# check1=$((sudo xcode-\select --install) 2>&1)
-# check2=$((sudo xcodebuild -license accept) 2>&1)
+# mac
+sudo softwareupdate --install --all
+check1=$((sudo xcode-\select --install) 2>&1)
+check2=$((sudo xcodebuild -license accept) 2>&1)
 
-# # show hidden files
-# defaults write com.apple.Finder AppleShowAllFiles true
+# show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
 
-# # git
-# brew install git
-# # see https://www.git-tower.com/blog/make-git-rebase-safe-on-osx
-# git config --global core.trustctime false
-# git config --global core.editor vim
-# git config --global core.excludesfile ~/.gitignore_global
-# git config --global user.name "Erich N Quintero"
-# git config --global user.email "qbantek@gmail.com"
+# git
+brew install git
+# see https://www.git-tower.com/blog/make-git-rebase-safe-on-osx
+git config --global core.trustctime false
+git config --global core.editor vim
+git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "Erich N Quintero"
+git config --global user.email "qbantek@gmail.com"
 
+# GitHub
+brew install gh
+gh config set editor vim
+gh config set git_protocol ssh --host github.com
 
-# # GitHub
-# brew install gh
-# gh config set editor vim
-# gh config set git_protocol ssh --host github.com
+# setup git ssh to GitHub
+ssh-keygen -t ed25519 -C $newgitemail
+ssh-add ~/.ssh/id_ed25519
+gh auth login
 
-# # setup git ssh to GitHub
-# ssh-keygen -t ed25519 -C $newgitemail
-# ssh-add ~/.ssh/id_ed25519
-# gh auth login
-
-# # setup gpg signed commits
-# echo "Adding a GPG key:"
-# echo "Supported GPG key algorithms: RSA, ElGamal, DSA, ECDH, ECDSA, EdDSA."
-# echo "Your key must be at least 4096 bits."
-# gpg --full-generate-key
-# gpg_key_id=`gpg --list-signatures --with-colons | grep 'sig' | grep "$newgitemail" | head -n 1 | cut -d':' -f5F`
-# ascii_key=`gpg --armor --export "$gpg_key_id"`
-# git config --global user.signingkey $gpg_key_id
-# git config --global commit.gpgsign true
-# gh gpg-key add $ascii_key
+# setup gpg signed commits
+echo "Adding a GPG key:"
+echo "Supported GPG key algorithms: RSA, ElGamal, DSA, ECDH, ECDSA, EdDSA."
+echo "Your key must be at least 4096 bits."
+gpg --full-generate-key
+gpg_key_id=`gpg --list-signatures --with-colons | grep 'sig' | grep "$newgitemail" | head -n 1 | cut -d':' -f5F`
+ascii_key=`gpg --armor --export "$gpg_key_id"`
+git config --global user.signingkey $gpg_key_id
+git config --global commit.gpgsign true
+gh gpg-key add $ascii_key
 
 # clone dotfiles from GitHub
 basedir="$HOME/dotfiles"
-repourl="git@github.com:qbantek/dotfiles.git"
-echo "Checking out dotfiles using git..."
 rm -rf "$basedir"
-git clone "$repourl" "$basedir"
+git clone git@github.com:qbantek/dotfiles.git "$basedir"
 cd "$basedir"
 git checkout mbpro
 
-# # zsh
-# brew install zsh
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-#   "$ZSH_CUSTOM/themes/powerlevel10k"
-# git clone https://github.com/zsh-users/zsh-autosuggestions \
-#   "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-#   "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+brew install gnupg  # make sure we have gpg available
+brew install iterm2 # preferred terminal
+brew install tree # directory structure as a 'tree'
+brew install bat  # cooler cat
+brew install ripgrep
+brew install fd   # simple, fast and user-friendly alternative to find
+brew install fzf  # fuzzy finder
+$(brew --prefix)/opt/fzf/install
+brew install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  "$ZSH_CUSTOM/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+git clone https://github.com/Aloxaf/fzf-tab \
+  "$ZSH_CUSTOM/plugins/fzf-tab"
 
-# # tmux
-# brew install tmux
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# tmux
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# # nvm 0.39.1 and latest lts node
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-# NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-# nvm install --lts
-# npm install --global npm@latest
-# npm install --global yarn
-# PATH="$PATH:`yarn global bin`"
+# nvm 0.39.1 and latest lts node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | sh
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
+npm install --global npm@latest
+npm install --global yarn
+PATH="$PATH:`yarn global bin`"
 
-# # go
-# brew install golang
+# go
+brew install golang
 
-# # python
-# brew install python
-# python3 -m pip install --upgrade pip
+# python
+brew install python
+python3 -m pip install --upgrade pip
 
-# # rvm
-# curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-# curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-# echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg2 --import-ownertrust # mpapis@gmail.com
-# echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg2 --import-ownertrust # piotr.kuczynski@gmail.com
-# \curl -sSL https://get.rvm.io | bash -s stable
-# source ~/.rvm/scripts/rvm
+# rvm
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg2 --import-ownertrust # mpapis@gmail.com
+echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg2 --import-ownertrust # piotr.kuczynski@gmail.com
+\curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
 
-# # neovim
-# brew install neovim --HEAD
-# sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-#   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-#   python3 -m pip install --user --upgrade pynvim
+# neovim
+brew install neovim --HEAD
+sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  python3 -m pip install --user --upgrade pynvim
 
-# # tools
-# brew install iterm2
-# brew install tree # directory structure as a 'tree'
-# brew install bat  # cooler cat
-# brew install ripgrep
-# brew install fd   # simple, fast and user-friendly alternative to find
-# brew install fzf  # fuzzy finder
-# sh -c '/opt/homebrew/opt/fzf/install'
-# brew install --cask visual-studio-code
-# brew install --cask docker
-# brew install --cask google-chrome
-# brew install --cask postman
+# tools
+
+brew install --cask visual-studio-code
+brew install --cask docker
+brew install --cask google-chrome
+brew install --cask postman
 
 # stow
-# brew install stow
+brew install stow
 
 rm -rf ~/.zshrc
 rm -rf ~/.config/nvim
 rm -rf ~/.rvm/gemsets/global.gems
 
-stow gem
+stow gem                 
 stow git
 stow nvim
 stow rvm
