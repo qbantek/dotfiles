42c5ef6e6c0ad6e87278c233b6a9a2f6e50e3907
Erich N Quintero <qbantek@gmail.com>
2022-06-09 17:44:15 -0400

    Remove unused

--- nvim/.config/nvim/lua/qbantek/lsp.lua
+++ nvim/.config/nvim/lua/qbantek/lsp.lua
@@ -24,21 +24,13 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})
   buf_set_keymap("n", "<space>sh", "<cmd>lua require('lspsaga.signature_help').signature_help()<cr>", opts)
   buf_set_keymap("n", "gr", "<cmd>lua require('lspsaga.provider').lsp_finder()<cr>", opts)
-  -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  -- buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  -- buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   -- buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   -- buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   -- buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   -- buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   -- buf_set_keymap('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   -- buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  -- buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<space>bf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
@@ -51,13 +43,12 @@ lsp_installer.on_server_ready(function(server)
   if server.name == "sumneko_lua" then
     opts.settings = {
       Lua = {
-        format = {
-          enable = true,
-        },
+        format = { enable = true },
         diagnostics = {
           enable = true,
           globals = { 'vim' },
-        }
+        },
+        telemetry = { enable = false },
       },
     }
   end
