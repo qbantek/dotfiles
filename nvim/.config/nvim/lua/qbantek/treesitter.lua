b7299f20dee8ee79d39dcbd793fb63e51ec2755c
Erich N Quintero <qbantek@gmail.com>
2022-06-12 02:26:07 -0400

    Rolled back

--- nvim/.config/nvim/lua/qbantek/treesitter.lua
+++ nvim/.config/nvim/lua/qbantek/treesitter.lua
@@ -24,15 +24,6 @@ require 'nvim-treesitter.configs'.setup {
   sync_install = false,
   highlight = {
     enable = true,
-    -- NOTE: these are the names of the parsers and not the filetype.
-    -- (for example if you want to disable highlighting for the `tex`
-    -- filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- NOTE: highlighting in dart is causing a segmentation fault! - 06/19/2022
-    disable = {
-      "dart"
-    },
     additional_vim_regex_highlighting = false,
   },
 }
