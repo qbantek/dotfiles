4b97a5702472d16ace1f521e45b00ea705c7c2a0
Erich N Quintero <qbantek@gmail.com>
2023-01-14 23:49:04 -0500

    Update setup for lspsaga 0.2.3+

--- nvim/.config/nvim/lua/qbantek/plugins/lsp/lspsaga.lua
+++ nvim/.config/nvim/lua/qbantek/plugins/lsp/lspsaga.lua
@@ -3,14 +3,24 @@ if not saga_status then
   return
 end
 
-saga.init_lsp_saga({
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-  -- use enter to open file with finder
+saga.setup({
+  move_in_saga = {
+    prev = "<C-k>",
+    next = "<C-j>"
+  },
+  error_sign = "",
+  warn_sign = "",
+  hint_sign = "",
+  infor_sign = "",
+  dianostic_header_icon = "   ",
+  finder_definition_icon = "  ",
+  finder_reference_icon = "  ",
   finder_action_keys = {
-    open = "<CR>",
+    open = "<CR>", -- default
   },
   -- use enter to open file with definition preview
   definition_action_keys = {
     edit = "<CR>",
   },
+  definition_preview_icon = "  ",
 })
