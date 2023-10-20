bcd377d35cc8c514299659f964a74efe9efd73ee
Erich N Quintero <qbantek@gmail.com>
2023-10-20 01:18:58 -0400

    Add plugins

--- nvim/.config/nvim/lua/qbantek/core/init.lua
+++ nvim/.config/nvim/lua/qbantek/core/init.lua
@@ -1,2 +1,3 @@
 require("qbantek.core.keymaps")
 require("qbantek.core.options")
+require("qbantek.core.autocmds")
--- nvim/.config/nvim/lua/qbantek/plugins/init.lua
+++ nvim/.config/nvim/lua/qbantek/plugins/init.lua
@@ -1,4 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "github/copilot.vim", -- copilot, need to say more?
+  "wakatime/vim-wakatime", -- automatic time-tracking
 }
