5f2ffddb163ecaa6bf673bc53a66d9a848fb059d
Erich N Quintero <qbantek@gmail.com>
2023-11-30 09:28:08 -0500

    Prevent folding text in Telescope results

--- nvim/.config/nvim/lua/qbantek/core/autocmds.lua
+++ nvim/.config/nvim/lua/qbantek/core/autocmds.lua
@@ -2,38 +2,44 @@ local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Highlight on yank
-augroup('YankHighlight', { clear = true })
-autocmd('TextYankPost', {
-  group = 'YankHighlight',
+augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+  group = "YankHighlight",
   callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '1000' })
-  end
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "1000" })
+  end,
 })
 
 -- Remove whitespace on save
-autocmd('BufWritePre', {
-  pattern = '',
-  command = ":%s/\\s\\+$//e"
+autocmd("BufWritePre", {
+  pattern = "",
+  command = ":%s/\\s\\+$//e",
 })
 
 -- Don't auto commenting new lines
-autocmd('BufEnter', {
-  pattern = '',
-  command = 'set fo-=c fo-=r fo-=o'
+autocmd("BufEnter", {
+  pattern = "",
+  command = "set fo-=c fo-=r fo-=o",
 })
 
 -- Enter insert mode when switching to terminal
-autocmd('TermOpen', {
-  command = 'setlocal listchars= nonumber norelativenumber nocursorline',
+autocmd("TermOpen", {
+  command = "setlocal listchars= nonumber norelativenumber nocursorline",
 })
 
-autocmd('TermOpen', {
-  pattern = '',
-  command = 'startinsert'
+autocmd("TermOpen", {
+  pattern = "",
+  command = "startinsert",
 })
 
 -- Close terminal buffer on process exit
-autocmd('BufLeave', {
-  pattern = 'term://*',
-  command = 'stopinsert'
+autocmd("BufLeave", {
+  pattern = "term://*",
+  command = "stopinsert",
+})
+
+-- Disable folding in Telescope's result window.
+autocmd("FileType", {
+  pattern = "TelescopeResults",
+  command = [[setlocal nofoldenable]],
 })
