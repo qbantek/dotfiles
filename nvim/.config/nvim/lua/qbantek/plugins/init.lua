786a834b38d29278a88de702d1779f362a7691b0
Erich N Quintero <qbantek@gmail.com>
2024-06-25 13:29:40 -0400

    Add configuration for Copilot

--- nvim/.config/nvim/lua/qbantek/plugins/init.lua
+++ nvim/.config/nvim/lua/qbantek/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  "github/copilot.vim", -- copilot, need to say more?
   "wakatime/vim-wakatime", -- automatic time-tracking
 }
